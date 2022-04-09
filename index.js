e270bf1f74384c54883a8e4a6db8384e18ab3d47
unknown <matthewaomalley@lewisu.edu>
2022-04-09 09:31:42 -0500

    basic user input to firebase operational

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //Noteify Final Project
 
+// import firebase module
 var fb = require('firebase')
 
-//initialize the app
+// initialize the app
 var appIni = fb.initializeApp({
   apiKey: "AIzaSyBQZNSXZK1EVIoez75smdeqEgS3yu3rwi0",
   authDomain: "noteify-d7193.firebaseapp.com",
@@ -11,23 +12,31 @@ var appIni = fb.initializeApp({
   storageBucket: "noteify-d7193.appspot.com",
   messagingSenderId: "259103554370",
   appId: "1:259103554370:web:bb31e247835601343266db",
-  measurementId: "G-DXJET6RER1"
-});
+  measurementId: "G-DXJET6RER1" });
 
-//
+// create firebase instance
 var firebaseDB = fb.database()
-
-// Theoretically add data to firebase 
-firebaseDB.ref("user").set("password")
 
 // use express and create app object
 const express = require('express')
 const app = express()
-
 var url = require('url')
 app.use(express.static(__dirname + '/static'))
 const port = process.env.PORT || 8080;
 
+// main function to send data to the firebase database
+app.get('/submitNote', (request, response) => {
+
+  // get title and note data from static page
+  var inputs = url.parse(request.url, true).query
+  const title = (inputs.title)
+  const note = (inputs.note)
+  // get username and put it as ref below
+
+  // send data to database
+  // user ref below will be replaced with username.
+  firebaseDB.ref("user2/").set({theTitle: title, theNote: note });
+})
 
 app.get('/login', (req, res) => {
 	var inputs = url.parse(request.url, true).query
